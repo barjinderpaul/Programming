3ed3377095216fa9e0a074ab2ee92a53587d9337
barjinderpaul <barjinderpaul.singh@gmail.com>
2019-11-15 15:32:05 +0530

    Done more practice in java

--- Java_Practice/src/com/company/assertions.java
+++ Java_Practice/src/com/company/assertions.java
@@ -3,9 +3,15 @@ package com.company;
 import java.util.Scanner;
 
 
+
 public class assertions {
 
     static void checkValidAgeToVoteWithAssertion(String name, int age) {
+
+        /**
+         * assertEquals(stringMessage,value,value is deprecated.)
+         */
+
         assert(age>=18);
         System.out.println("Eligible to vote");
     }
@@ -13,7 +19,7 @@ public class assertions {
     static void checkValidAgeToVoteWithException(String name, int age) {
         try {
             if(age < 18){
-                throw   new Exception("Not eligble to vote");
+                throw new Exception("Not eligble to vote");
             }
         }
         catch(Exception e) {
@@ -21,6 +27,40 @@ public class assertions {
         }
     }
 
+    static void checkingWithAssertSame() {
+        Integer original = new Integer(1);
+        Integer same = original;
+        boolean result = same == original;
+        System.out.println("Equal or not = " + result );
+        original++;
+        result = same == original;
+        System.out.println("Equal or not = " + result );
+        
+        original++;
+        System.out.println("same = " + same + " original" + original );
+        original = same;
+        System.out.println("same = " + same + " original" + original );
+        System.out.println("size of same = " + same.SIZE/8 + " size of original = " + original.SIZE/8);
+        Integer diff = new Integer(1);
+        System.out.println(original + same + diff);
+        // Assertion.assertEquals(original,same);
+    }
+
+    static void ifBlock(){
+        if( (1|1) ==1  ){
+            System.out.println("Truee");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+
+    static void checking() {
+        int a = 1;
+        int b = a;
+        a++;
+        System.out.println("a,b = "+ a + " " + b);
+    }
     public static void main(String  args[]) {
 
         // practicing assertions;
@@ -30,7 +70,16 @@ public class assertions {
         
         checkValidAgeToVoteWithException(name,age);
 
-        checkValidAgeToVoteWithAssertion(name,age);
+        // checkValidAgeToVoteWithAssertion(name,age);
+
+
+        checkingWithAssertSame();
+
+        checking();
+
+        ifBlock();
+
+        
 
     }
 }
